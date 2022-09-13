420f943e355010b3dae7cfdff856f06e66909b53
Brandon Castro <castroyou999@gmail.com>
2022-09-12 22:15:59 -0400

    commit tree 6973a672a4f2e2b539dc1ffe95a2af6757103f80

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names,event){
+let eventArray=[];
+    for (let i = 0; i<names.length;i++){
+eventArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+}
+return eventArray;
+}
+function countDown(number){
+    while(0<=number){
+        console.log(number);
+        number--;
+    }
+}
